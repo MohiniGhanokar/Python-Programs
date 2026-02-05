ba3668d0c583e0729e836959cd08c3767d09f3c7
MohiniGhanokar <mohinighanokar3@gmail.com>
2026-02-05 21:17:53 +0530

    Update StudentPassFail.py

--- StudentPassFail.py
+++ StudentPassFail.py
@@ -1,7 +1,8 @@
-English_marks=float(input("Enter student marks :"))
-math_marks=float(input("Enter student marks :"))
-Chemistry_marks=float(input("Enter student marks :"))
-if English_marks>=35 and math_marks>=35 and Chemistry_marks>=35 :
+Physics_marks=float(input("Enter student marks of Physics :"))
+math_marks=float(input("Enter student marks of Math :"))
+Chemistry_marks=float(input("Enter student marks of Chemistry :"))
+if Physics_marks>=35 and math_marks>=35 and Chemistry_marks>=35 :
     print("Student have Passed the exam!")
 else:
     print("Student have failed in exams!")    
+

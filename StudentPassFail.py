e568e71247c7cbb5c566ef51c23aa89c5f5f5f5d
MohiniGhanokar <mohinighanokar3@gmail.com>
2026-02-05 21:12:19 +0530

    Added

---
+++ StudentPassFail.py
@@ -0,0 +1,7 @@
+English_marks=float(input("Enter student marks :"))
+math_marks=float(input("Enter student marks :"))
+Chemistry_marks=float(input("Enter student marks :"))
+if English_marks>=35 and math_marks>35 and Chemistry_marks :
+    print("Student have Passed the exam!")
+else:
+    print("Student have failed in exams!")    

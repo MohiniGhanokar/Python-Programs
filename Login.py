59c893f9735bc525cc315a023e53dfbb4fd23afe
MohiniGhanokar <mohinighanokar3@gmail.com>
2026-02-05 21:11:18 +0530

    User Login

---
+++ Login.py
@@ -0,0 +1,6 @@
+username=input("enter username for login :")
+password=int(input("enter password for login :"))
+if username=="Monu" and password==1234:
+    print("Successfully Login!")    
+else:
+    print("Login Unsuccessfull!")    
